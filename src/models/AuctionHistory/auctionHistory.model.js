ebbbc3e06155d2261ddc135ceb8bd2dfb95376e9
DESKTOP-QA4996B\inau <ptmhau17ck1@gmail.com>
2021-10-31 00:22:47 +0700

    create model auctionhistory, rate, tradehistory

---
+++ src/models/AuctionHistory/auctionHistory.model.js
@@ -0,0 +1,49 @@
+const db = require("../../utils/db");
+const Sequelize = require("sequelize");
+const Model = Sequelize.Model;
+const DataTypes = Sequelize.DataTypes;
+const moment = require("moment");
+moment().utcOffset("+07:00");
+
+class AuctionHistory extends Model {}
+
+AuctionHistory.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    last_bid_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      get: function () {
+        return moment(this.getDataValue("last_bid_at")).format(
+          "DD/MM/YYYY hh:mm:ss"
+        );
+      },
+    },
+  },
+  {
+    sequelize: db,
+    modelName: "AuctionHistory",
+  }
+);
+
+module.exports = AuctionHistory;
--- src/utils/db.js
+++ src/utils/db.js
@@ -1,33 +1,33 @@
-const Sequelize = require('sequelize');
-const fs = require('fs');
-const path = '../../public/certificates/BaltimoreCyberTrustRoot.crt.pem';
+const Sequelize = require("sequelize");
+const fs = require("fs");
+const path = "../../public/certificates/BaltimoreCyberTrustRoot.crt.pem";
 
-const serverCa = [ fs.readFileSync(require.resolve(path), 'utf8') ];
+const serverCa = [fs.readFileSync(require.resolve(path), "utf8")];
 
 //Connection String
 const db = new Sequelize({
-	dialect: process.env.DB_SQL_TYPE,
-	host: process.env.DB_IP,
-	port: Number(process.env.DB_PORT),
-	username: process.env.DB_USERNAME,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_NAME,
-	dialectOptions: {
-		ssl: {
-			rejectUnauthorized: true,
-			ca: serverCa
-		}
-	},
-	timezone: process.env.DB_TIMEZONE,
-	define: {
-		freezeTableName: true,
-		timestamps: false
-	},
-	pool: {
-		max: Number(process.env.DB_MAX_CONNECT),
-		min: Number(process.env.DB_MIN_CONNECT),
-		idle: 5 * 1000 //milliseconds
-	}
+  dialect: process.env.DB_SQL_TYPE,
+  host: process.env.DB_IP,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: true,
+      ca: serverCa,
+    },
+  },
+  timezone: process.env.DB_TIMEZONE,
+  define: {
+    freezeTableName: true,
+    timestamps: false,
+  },
+  pool: {
+    max: Number(process.env.DB_MAX_CONNECT),
+    min: Number(process.env.DB_MIN_CONNECT),
+    idle: 5 * 1000, //milliseconds
+  },
 });
 
 module.exports = db;
