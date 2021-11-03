21a4ed53cbde963caf0e21fe4e7390e957939021
xizot <nvnhat.17ck1@gmail.com>
2021-11-03 22:14:13 +0700

    rate controller get by id

--- src/routes/rate/rate.controller.js
+++ src/routes/rate/rate.controller.js
@@ -1,5 +1,18 @@
-const rate_service = require('../../models/Rate/rate.service');
+const { getTotalPage } = require("../../helpers/etc.helper");
+const rate_service = require("../../models/Rate/rate.service");
 
 module.exports.get = async (req, res) => {
-	return res.json('ok');
+  return res.json("ok");
+};
+
+module.exports.getByUserId = async (req, res) => {
+  const { id, page, limit } = req.query;
+  const rs = {};
+  const list = await rate_service.getActiveRate(id, [], page, limit);
+  rs.count = list.count;
+  rs.data = list.rows;
+  rs.page = +page;
+  rs.total_page = getTotalPage(rs.count, limit);
+
+  return res.json(rs);
 };
---
+++ src/schema/rate.schema.js
@@ -0,0 +1,14 @@
+const regex_pattern = require("../constants/regex_pattern.constant");
+
+exports.rateSchema = {
+  type: "object",
+  properties: {
+    point: { type: "string", minLength: 1 },
+    user2: { type: "string", minLength: 5 },
+    comment: { type: "string", minLength: 5 },
+    product_id: { type: "string", minLength: 5 },
+  },
+
+  required: ["point", "user2", "product_id"],
+  additionalProperties: true,
+};
