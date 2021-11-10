7152c3b2a2ed166e6e8936cf97607fa2f3ecdfc0
xizot <20424058@student.hcmus.edu.vn>
2021-11-10 16:34:32 +0700

    fixbug validator

--- src/schema/product.schema.js
+++ src/schema/product.schema.js
@@ -9,19 +9,10 @@ exports.createProductSchema = {
 		detail: { type: 'string', minLength: 5 },
 		start_price: { type: 'number', minimum: 1000 },
 		step_price: { type: 'number', minimum: 1000 },
-		buy_price: { type: 'number', minimum: 2000 },
 		expire_at: { type: 'string', pattern: regex_pattern.dateTimePattern }
 	},
 
-	required: [
-		'sub_category_id',
-		'name',
-		'auto_extend',
-		'detail',
-		'start_price',
-		'step_price',
-		'expire_at'
-	],
+	required: [ 'sub_category_id', 'name', 'auto_extend', 'detail', 'start_price', 'step_price', 'expire_at' ],
 	additionalProperties: true
 };
 
