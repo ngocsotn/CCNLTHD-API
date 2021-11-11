f1113a738a6906fb7e4ac5f67bbfe6958f94a1a8
inauu <ptmhau17ck1@gmail.com>
2021-11-11 13:52:59 +0700

    add extra info when update profile

--- src/routes/admin/admin.controller.js
+++ src/routes/admin/admin.controller.js
@@ -1,44 +1,59 @@
-const user_service = require('../../models/user/user.service');
-const http_message = require('../../constants/http_message.constant');
-const { handlePagingResponse } = require('../../helpers/etc.helper');
+const user_service = require("../../models/user/user.service");
+const http_message = require("../../constants/http_message.constant");
+const { handlePagingResponse } = require("../../helpers/etc.helper");
 
 module.exports.getAllUser = async (req, res) => {
-	const { limit, page } = req.query;
-	const list = await user_service.getAllUser([ 'password', 'code', 'refresh_token' ], page, limit);
-	const rs = handlePagingResponse(list, page, limit);
+  const { limit, page } = req.query;
+  const list = await user_service.getAllUser(
+    ["password", "code", "refresh_token"],
+    page,
+    limit
+  );
+  const rs = handlePagingResponse(list, page, limit);
 
-	return res.json(rs);
+  return res.json(rs);
 };
 
 module.exports.getUserDetails = async (req, res) => {
-	const user_id = req.params.id;
-	const rs = await user_service.findUserById(user_id, [ 'password', 'code', 'refresh_token' ]);
-	if (!rs) {
-		return res.status(204).json({});
-	}
+  const user_id = req.params.id;
+  const rs = await user_service.findUserById(user_id, [
+    "password",
+    "code",
+    "refresh_token",
+  ]);
+  if (!rs) {
+    return res.status(204).json({});
+  }
 
-	return res.json(rs);
+  return res.json(rs);
 };
 
 module.exports.updateUser = async (req, res) => {
-	const { birth, email, name, address, user_id } = req.body;
-	const err_message = [];
+  const { birth, email, name, address, user_id } = req.body;
+  const err_message = [];
 
-	if (name) {
-		await user_service.updateName(user_id, name);
-	}
-	if (address) {
-		await user_service.updateAddress(user_id, address);
-	}
-	if (birth) {
-		await user_service.updateBirth(user_id, birth);
-	}
-	if (email) {
-		const rs = await user_service.updateEmail(user_id, email);
-		if (!rs) {
-			err_message.push(http_message.status401_conflict_email.message);
-		}
-	}
+  if (name) {
+    await user_service.updateName(user_id, name);
+  }
+  if (address) {
+    await user_service.updateAddress(user_id, address);
+  }
+  if (birth) {
+    await user_service.updateBirth(user_id, birth);
+  }
+  if (email) {
+    const rs = await user_service.updateEmail(user_id, email);
+    if (!rs) {
+      err_message.push(http_message.status401_conflict_email.message);
+    }
+  }
 
-	return res.status(200).json({ message: http_message.status200.message, errs: err_message });
+  const rs = await user_service.findUserById(user_id, [
+    "password",
+    "code",
+    "refresh_token",
+  ]);
+  rs.dataValues.errs = err_message;
+
+  return res.status(200).json(rs);
 };
--- src/routes/profile/profile.controller.js
+++ src/routes/profile/profile.controller.js
@@ -1,48 +1,57 @@
-const http_message = require('../../constants/http_message.constant');
-const bcrypt_helper = require('../../helpers/bcrypt.helper');
-const user_service = require('../../models/user/user.service');
+const http_message = require("../../constants/http_message.constant");
+const bcrypt_helper = require("../../helpers/bcrypt.helper");
+const user_service = require("../../models/user/user.service");
 
+// tự get thông tin cá nhân
 module.exports.profileGet = async (req, res) => {
-	const payload = req.token;
-	const rs = await user_service.findUserById(payload.id, [ 'password', 'code', 'refresh_token' ]);
+  const token = req.token;
+  const rs = await user_service.findUserById(token.id, [
+    "password",
+    "code",
+    "refresh_token",
+  ]);
 
-	return res.status(200).json(rs);
+  return res.status(200).json(rs);
 };
 
+// thay đổi thông tin cá nhân, truyền gì thì thay đổi đó
 module.exports.profilePut = async (req, res) => {
-	const payload = req.token;
-	const { name, email, address, birth, password, new_password } = req.body;
-	const err_message = [];
+  const token = req.token;
+  const { name, email, address, birth, password, new_password } = req.body;
+  const err_message = [];
 
-	if (name) {
-		await user_service.updateName(payload.id, name);
-	}
-	if (address) {
-		await user_service.updateAddress(payload.id, address);
-	}
-	if (birth) {
-		await user_service.updateBirth(payload.id, birth);
-	}
+  if (name) {
+    await user_service.updateName(token.id, name);
+  }
+  if (address) {
+    await user_service.updateAddress(token.id, address);
+  }
+  if (birth) {
+    await user_service.updateBirth(token.id, birth);
+  }
 
-	if (password && new_password) {
-		if (new_password.length < 5) {
-			err_message.push(http_message.status400_short_password.message);
-		} else {
-			const rs = await user_service.findUserById(payload.id, []);
-			if (!await bcrypt_helper.verify(password, rs.password)) {
-				err_message.push(http_message.status401_wrong_password.message);
-			} else {
-				await user_service.updatePassword(payload.id, new_password);
-			}
-		}
-	}
+  if (password && new_password) {
+    if (new_password.length < 5) {
+      err_message.push(http_message.status400_short_password.message);
+    } else {
+      const rs = await user_service.findUserById(token.id, []);
+      if (!(await bcrypt_helper.verify(password, rs.password))) {
+        err_message.push(http_message.status401_wrong_password.message);
+      } else {
+        await user_service.updatePassword(token.id, new_password);
+      }
+    }
+  }
 
-	if (email) {
-		const rs = await user_service.updateEmail(payload.id, email);
-		if (!rs) {
-			err_message.push(http_message.status401_conflict_email.message);
-		}
-	}
+  if (email) {
+    const rs = await user_service.updateEmail(token.id, email);
+    if (!rs) {
+      err_message.push(http_message.status401_conflict_email.message);
+    }
+  }
 
-	return res.status(200).json({ message: http_message.status200.message, errs: err_message });
+  const rs = await user_service.findUserById(token.id, ['password', 'refresh_token', 'code']);
+  rs.dataValues.errs = err_message;
+
+  return res.status(200).json(rs);
 };
